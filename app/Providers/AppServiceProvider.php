d8ee6cdc1d0fd4713d127146564fe9dc570f5aa2
sugitakai <78083472+sugitakai@users.noreply.github.com>
2024-02-12 13:56:22 +0900

    スペル修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (\App::enviroment(['production'])){
+        if (\App::environment(['production'])){
             \URL::forseScheme('https');
         }
     }
